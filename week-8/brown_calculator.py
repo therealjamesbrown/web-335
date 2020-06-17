ba16b4e19452789dffe1b99c3030676f442f9712
therealjamesbrown <jamesbbb1@gmail.com>
2020-06-17 10:30:37 -0500

    updating

---
+++ week-8/brown_calculator.py
@@ -0,0 +1,23 @@
+""" 
+============================================
+; Title:  Exercise 8.3
+; Author: Professor Krasso
+; Date:   6/17/2020
+; Description: coding per requirements of exercise 8.3
+;===========================================
+""" 
+
+print("--Welcome to assignment 8.3--")
+
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def divide(num1, num2):
+    return num1 / num2
+
+print(add(1,2))
+print(subtract(4,1))
+print(divide(8,2))
