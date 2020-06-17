be743cfd22b3e4db9b103347919bee5594b9a761
therealjamesbrown <jamesbbb1@gmail.com>
2020-06-17 10:09:14 -0500

    adding code attribution

--- week-8/brown_hello_world.py
+++ week-8/brown_hello_world.py
@@ -1,1 +1,12 @@
+""" 
+/*
+============================================
+; Title:  Exercise 8.2
+; Author: James Brown
+; Date:   6/17/2020
+; Description: coding per requirements of exercise 8.2
+;===========================================
+*/
+""" 
+
 print('Hello World')
